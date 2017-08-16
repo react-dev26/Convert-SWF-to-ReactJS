cd6bb5c5985b973879f1e53a056df4283f1e8ea6
Crazy Dev <cool.dev0206@gmail.com>
2017-08-16 00:43:24 -0400

    add slide component ( progress, style)

--- src/components/options/OutlineContent/index.js
+++ src/components/options/OutlineContent/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import styles from './styles';
+
 function OutlineContent() {
   return (
-    <div>
-      outline
+    <div style={styles.containers}>
+      <ul style={styles.titleContainer}>
+        <li style={styles.title}>Slide Title</li>
+        <li style={styles.title}>Duration</li>
+      </ul>
     </div>
   );
 }
