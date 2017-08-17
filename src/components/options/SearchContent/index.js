c8e7a0f9a26f597ba8ad73e4b328c854f55d86e4
Crazy Dev <cool.dev0206@gmail.com>
2017-08-17 04:28:13 -0400

    complete search filtering ( function, style )

--- src/components/options/SearchContent/index.js
+++ src/components/options/SearchContent/index.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import SearchInput, {createFilter} from 'react-search-input'
 
-function OutlineContent() {
-  return (
-    <div>
-      search
-    </div>
-  );
+import data from './filterTestingData';
+import styles from './styles';
+
+const KEYS_TO_FILTERS = ['product.name', 'id'];
+
+class OutlineContent extends Component {
+  constructor() {
+    super();
+    this.state = { searchTerm: ''};
+    this.searchUpdated = this.searchUpdated.bind(this);
+  }
+  searchUpdated(term) {
+   this.setState({searchTerm: term})
+  }
+  render() {
+    const filterProduct = data.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS))
+
+    return (
+      <div style={styles.containers}>
+        <ul style={styles.titleContainer}>
+          <li style={styles.title}>Search for text:</li>
+        </ul>
+        <div id="filtering">
+          <div className="searchBox">
+            <SearchInput className='search-input' onChange={this.searchUpdated} />
+            <button ><i className="material-icons" >search</i></button>
+          </div>
+          <ul style={styles.titleContainer}>
+            <li style={styles.title}>Slide Title</li>
+            <li style={styles.title}>Duration</li>
+          </ul>
+          {filterProduct.map(item => {
+            return (
+              <div className='mail' style={{display: 'none'}} key={item.id}>
+                <div className='from'>{item.product.name}</div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default OutlineContent;
